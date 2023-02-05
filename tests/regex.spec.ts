589431baa8a02cd5d9090f19e372ea45c33b085d
talatkuyuk <talatkuyuk@gmail.com>
2023-02-05 23:27:13 +0300

    added test files

---
+++ tests/regex.spec.ts
@@ -0,0 +1,102 @@
+import { REGEX_START } from "../src";
+
+type Fixture = {
+  input: string;
+  expect: null | {
+    type: undefined | string;
+    title: undefined | string;
+  };
+};
+
+describe("remark-container", () => {
+  it("REGEX_CUSTOM_CONTAINER matches with custom container", () => {
+    const fixtures: Fixture[] = [
+      {
+        input: ":::",
+        expect: {
+          type: undefined,
+          title: undefined,
+        },
+      },
+      {
+        input: ":::     ",
+        expect: {
+          type: undefined,
+          title: undefined,
+        },
+      },
+      {
+        input: ":: : info",
+        expect: null,
+      },
+      {
+        input: ":::warning",
+        expect: {
+          type: "warning",
+          title: undefined,
+        },
+      },
+      {
+        input: "::: warning",
+        expect: {
+          type: "warning",
+          title: undefined,
+        },
+      },
+      {
+        input: ":::    warning   ",
+        expect: {
+          type: "warning",
+          title: undefined,
+        },
+      },
+      {
+        input: "::: danger Title",
+        expect: {
+          type: "danger",
+          title: "Title",
+        },
+      },
+      {
+        input: ":::    danger        Title  ",
+        expect: {
+          type: "danger",
+          title: "Title",
+        },
+      },
+      {
+        input: "::: danger My Title",
+        expect: {
+          type: "danger",
+          title: "My Title",
+        },
+      },
+      {
+        input: ":::    danger    My   Title  'flexible'   ",
+        expect: {
+          type: "danger",
+          title: "My   Title  'flexible'",
+        },
+      },
+    ];
+
+    fixtures.forEach((fixture, index) => {
+      const match = fixture.input.match(REGEX_START);
+
+      console.log(index);
+
+      if (fixture.expect === null) {
+        expect(match).toBeNull();
+      } else {
+        expect(match).not.toBeNull();
+      }
+
+      if (match) {
+        const [_, __, type, title] = match;
+
+        expect(type).toBe(fixture.expect?.type);
+        expect(title).toBe(fixture.expect?.title);
+      }
+    });
+  });
+});
---
+++ tests/without_options.spec.ts
@@ -0,0 +1,142 @@
+import { unified, type Processor } from "unified";
+import remarkParse from "remark-parse";
+import gfm from "remark-gfm";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+import dedent from "dedent";
+import type { VFileCompatible } from "vfile";
+
+import plugin from "../src";
+
+const compiler: Processor = unified()
+  .use(remarkParse)
+  .use(gfm)
+  .use(plugin)
+  .use(remarkRehype)
+  .use(rehypeStringify);
+
+const process = async (contents: VFileCompatible): Promise<VFileCompatible> => {
+  return compiler.process(contents).then((file) => file.value);
+};
+
+describe("no options - fail", () => {
+  // ******************************************
+  it("No type, no title, no content", async () => {
+    const input = dedent`
+      :::
+      
+      :::
+    `;
+
+    expect(await process(input)).toMatchInlineSnapshot(`
+      "<p>:::</p>
+      <p>:::</p>"
+    `);
+  });
+
+  // ******************************************
+  it("Type mis-placed, no title, no content", async () => {
+    const input = dedent`
+        :::
+        
+        ::: tip
+      `;
+
+    expect(await process(input)).toMatchInlineSnapshot(`
+      "<p>:::</p>
+      <p>::: tip</p>"
+    `);
+  });
+});
+
+describe("no options - success", () => {
+  // ******************************************
+  it("No type, no title, with content", async () => {
+    const input = dedent`
+      :::
+
+      content
+      
+      :::
+    `;
+
+    expect(await process(input)).toMatchInlineSnapshot(
+      `"<div class="remark-container"><p>content</p></div>"`,
+    );
+  });
+
+  // ******************************************
+  it("Type is defined, no title, no content", async () => {
+    const input = dedent`
+      ::: info
+      
+      :::
+    `;
+
+    expect(await process(input)).toMatchInlineSnapshot(
+      `"<div class="remark-container info"></div>"`,
+    );
+  });
+
+  // ******************************************
+  it("Type is defined, no title, with content", async () => {
+    const input = dedent`
+      ::: danger
+
+      content
+      
+      :::
+    `;
+
+    expect(await process(input)).toMatchInlineSnapshot(
+      `"<div class="remark-container danger"><p>content</p></div>"`,
+    );
+  });
+
+  // ******************************************
+  it("All type, title and content are defined", async () => {
+    const input = dedent`
+      ::: danger Title
+
+      content
+      
+      :::
+    `;
+
+    expect(await process(input)).toMatchInlineSnapshot(
+      `"<div class="remark-container danger"><div class="remark-container-title danger">Title</div><p>content</p></div>"`,
+    );
+  });
+
+  // ******************************************
+  it("All type, title and content are defined; with extreme spaces", async () => {
+    const input = dedent`
+        :::danger      My      Title       
+  
+        content
+        
+        :::
+      `;
+
+    expect(await process(input)).toMatchInlineSnapshot(
+      `"<div class="remark-container danger"><div class="remark-container-title danger">My Title</div><p>content</p></div>"`,
+    );
+  });
+
+  // ******************************************
+  it("All type, title and content are defined, more content", async () => {
+    const input = dedent`
+        ::: danger Title
+  
+        **bold text** paragraph
+
+        other paragraph *italic content*
+        
+        :::
+      `;
+
+    expect(await process(input)).toMatchInlineSnapshot(
+      `"<div class="remark-container danger"><div class="remark-container-title danger">Title</div><p><strong>bold text</strong> paragraph</p><p>other paragraph <em>italic content</em></p></div>"`,
+    );
+  });
+});
